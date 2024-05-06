746f75222cd4354304e53060e0190ee9f211262e
tmnpierre <tomyn.pierre@gmail.com>
2024-05-06 12:47:22 +0200

    feat(core): automatisation

--- main.cbl
+++ main.cbl
@@ -1,15 +1,89 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. Main.
+       PROGRAM-ID. CREATEGROUPVARIABLES.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+      
+      *    Sélectionne le fichier d'entrée.
+           SELECT INPUTFILE ASSIGN TO "input.txt"    
+               ORGANIZATION IS LINE SEQUENTIAL.
+      
+      *    Sélectionne le fichier de sortie.
+           SELECT OUTPUTFILE ASSIGN TO "output.cpy"  
+               ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
+       FD INPUTFILE.
+      
+      *    Définit la structure de l'enregistrement d'entrée.
+       01 INPUTRECORD PIC X(200).
+
+       FD OUTPUTFILE.
+      *    Définit la structure de l'enregistrement de sortie.
+       01 OUTPUTRECORD PIC X(200). 
 
        WORKING-STORAGE SECTION.
 
-       COPY 'Exemple.cpy'.
+      *    Indicateur de fin de fichier.
+       01 WS-EOF PIC X VALUE 'N'.
+      *    Nom de champ.    
+       01 WS-FIELD-NAME PIC X(30).   
+       01 WS-FIELD-VALUE PIC X(100). 
+       01 WS-PIC-STATEMENT PIC X(100). 
+       01 WS-START-POS PIC 99.
+       01 WS-END-POS PIC 99..
+       01 WS-FILLER PIC X(5) VALUE SPACES. 
 
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-           DISPLAY 'Chargement des données à partir de la clause COPY'.
-           DISPLAY EXERCICE-ENTETE.
+       BEGIN.
+           OPEN INPUT INPUTFILE                    
+                OUTPUT OUTPUTFILE.
+
+           MOVE SPACES TO OUTPUTRECORD  
+      *    Ajoute la déclaration de groupe.          
+           MOVE '01 MY-GROUP.' TO OUTPUTRECORD(8:17)  
+           WRITE OUTPUTRECORD
+               AFTER ADVANCING 1 LINE
+
+           * Boucle de traitement jusqu'à la fin du fichier.
+           PERFORM UNTIL WS-EOF = 'Y'
+               READ INPUTFILE
+                   AT END MOVE 'Y' TO WS-EOF
+               END-READ
+               PERFORM PROCESS-LINE
+           END-PERFORM.
+
+           CLOSE INPUTFILE
+                 OUTPUTFILE
            STOP RUN.
+
+       PROCESS-LINE.
+           MOVE 1 TO WS-START-POS
+      *    Boucle jusqu'à la fin de la ligne.
+           PERFORM UNTIL WS-START-POS > FUNCTION LENGTH(INPUTRECORD)
+               PERFORM FIND-NEXT-WORD           
+               PERFORM WRITE-PIC-STATEMENT     
+           END-PERFORM.
+
+       FIND-NEXT-WORD.
+      * Définit la position de fin comme la longueur de la ligne.
+           MOVE FUNCTION LENGTH(INPUTRECORD) TO WS-END-POS
+      * Compte le nombre d'espaces pour trouver le prochain mot.        
+           INSPECT INPUTRECORD TALLYING WS-END-POS FOR ALL SPACES  
+      * Calcule la longueur du mot.
+           COMPUTE WS-FIELD-NAME = WS-END-POS - WS-START-POS 
+      * Extrait et convertit le mot en majuscules.     
+           MOVE FUNCTION UPPER-CASE(INPUTRECORD(WS-START-POS:
+                         LENGTH(WS-FIELD-NAME))) TO WS-FIELD-VALUE 
+      * Met à jour la position de départ pour la prochaine recherche.
+           MOVE WS-END-POS TO WS-START-POS                         
+           ADD 1 TO WS-START-POS.
+
+       WRITE-PIC-STATEMENT.
+           STRING '   05 ' WS-FIELD-NAME ' PIC X(' WS-FIELD-NAME 
+                  ') VALUE ' WS-FIELD-VALUE INTO WS-PIC-STATEMENT
+           STRING WS-PIC-STATEMENT WS-FILLER INTO OUTPUTRECORD    
+           WRITE OUTPUTRECORD 
+               AFTER ADVANCING 1 LINE.                             
---
+++ output.cpy
@@ -0,0 +1,36 @@
+       01 MY-GROUP.
+           05 IDENTIFIANTCT     PIC X(14) VALUE 'IdentifiantCT'.
+           05 FILLER            PIC X(5).
+           05 MUNA              PIC X(4) VALUE 'Muna'.
+           05 FILLER            PIC X(5).
+           05 IDENTIFIANTSE     PIC X(14) VALUE 'IdentifiantSE'.
+           05 FILLER            PIC X(5).
+           05 MUNASE            PIC X(6) VALUE 'MunaSE'.
+           05 FILLER            PIC X(5).
+           05 IDENTIFIANTRG     PIC X(14) VALUE 'IdentifiantRG'.
+           05 FILLER            PIC X(5).
+           05 MUNARG            PIC X(6) VALUE 'MunaRG'.
+           05 FILLER            PIC X(5).
+           05 LIBELLÉ           PIC X(20) VALUE 'Libellé'.
+           05 FILLER            PIC X(5).
+           05 MOTCLÉ            PIC X(20) VALUE 'MotClé'.
+           05 FILLER            PIC X(5).
+           05 LISSABLE          PIC X(8) VALUE 'Lissable'.
+           05 FILLER            PIC X(5).
+           05 TVA               PIC X(4) VALUE 'TVA'.
+           05 FILLER            PIC X(5).
+           05 CMU               PIC X(4) VALUE 'CMU'.
+           05 FILLER            PIC X(5).
+           05 ETAT              PIC X(6) VALUE 'Etat'.
+           05 FILLER            PIC X(5).
+           05 TYPERG            PIC X(8) VALUE 'TypeRG'.
+           05 FILLER            PIC X(5).
+           05 SOUSTYPE          PIC X(12) VALUE 'Soustype'.
+           05 FILLER            PIC X(5).
+           05 DATEMAJ           PIC X(10) VALUE 'DateMAJ'.
+           05 FILLER            PIC X(5).
+           05 DÉBUTEFFET        PIC X(10) VALUE 'DébutEffet'.
+           05 FILLER            PIC X(5).
+           05 FINEFFET          PIC X(10) VALUE 'FinEffet'.
+           05 FILLER            PIC X(5).
+           05 PRIORITÉ          PIC X(8) VALUE 'Priorité'.
